1b2663791b3119eaba36d10ad771f7465510d23a
sanghoon <tkdgns5817@gmail.com>
2025-03-19 22:23:23 +0900

    feat(#84): StudyCurriculum 도메인 개발
    - createOrUpdateOrDelete 개발
    - 다 건 조회 개발
    - 단 건 조회 개발
    - tc 작성

--- src/main/java/com/mos/backend/studycurriculum/infrastructure/StudyCurriculumJpaRepository.java
+++ src/main/java/com/mos/backend/studycurriculum/infrastructure/StudyCurriculumJpaRepository.java
@@ -1,7 +1,14 @@
 package com.mos.backend.studycurriculum.infrastructure;
 
+import com.mos.backend.studies.entity.Study;
 import com.mos.backend.studycurriculum.entity.StudyCurriculum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudyCurriculumJpaRepository extends JpaRepository<StudyCurriculum, Long> {
+    List<StudyCurriculum> findAllByStudy(Study study);
+
+    Optional<StudyCurriculum> findByIdAndStudy(Long id, Study study);
 }
--- src/main/java/com/mos/backend/studycurriculum/infrastructure/StudyCurriculumRepository.java
+++ src/main/java/com/mos/backend/studycurriculum/infrastructure/StudyCurriculumRepository.java
@@ -1,9 +1,19 @@
 package com.mos.backend.studycurriculum.infrastructure;
 
+import com.mos.backend.studies.entity.Study;
 import com.mos.backend.studycurriculum.entity.StudyCurriculum;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface StudyCurriculumRepository {
     void saveAll(List<StudyCurriculum> studyCurriculumList);
+
+    List<StudyCurriculum> findAllByStudy(Study study);
+
+    void deleteAll(List<StudyCurriculum> studyCurriculumList);
+
+    void save(StudyCurriculum studyCurriculum);
+
+    Optional<StudyCurriculum> findByIdAndStudy(Long id, Study study);
 }

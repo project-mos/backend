0112378fd9c86878289573312289e79f8f7b808e
sanghoon <tkdgns5817@gmail.com>
2025-10-15 18:29:09 +0900

    refactor(#115): 알림 메시지 발송을 위한 데이터 객체 생성 방식 수정
    - 기존 여러 데이터 객체를 생성해서 반복 발송 -> 유저 아이디 목록을 통해 한 번에 발송 처리

--- src/main/java/com/mos/backend/notifications/application/eventhandler/NotificationEventHandler.java
+++ src/main/java/com/mos/backend/notifications/application/eventhandler/NotificationEventHandler.java
@@ -4,10 +4,8 @@ import com.mos.backend.common.event.EventType;
 import com.mos.backend.common.event.NotificationPayload;
 import com.mos.backend.notifications.application.dto.NotificationDetails;
 
-import java.util.List;
-
 public interface NotificationEventHandler<T extends NotificationPayload> {
-    List<NotificationDetails> prepareDetails(EventType type, T payload);
+    NotificationDetails prepareDetails(EventType type, T payload);
     EventType support();
 }
 

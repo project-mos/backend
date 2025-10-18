3129191d77edc89bdb93d7fd8392f52463f092c3
sanghoon <tkdgns5817@gmail.com>
2025-10-18 19:05:58 +0900

    fix(#115): 스터디 멤버 추가 시 알림 처리
    json을 객체로 역직렬화를 위해 기본 생성자 추가

--- src/main/java/com/mos/backend/notifications/application/dto/payload/NewStudyMemberPayload.java
+++ src/main/java/com/mos/backend/notifications/application/dto/payload/NewStudyMemberPayload.java
@@ -2,9 +2,11 @@ package com.mos.backend.notifications.application.dto.payload;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewStudyMemberPayload extends DataPayload{
     private Long studyId;
 }
